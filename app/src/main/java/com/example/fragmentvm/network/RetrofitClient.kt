c3e6232765d2ef70befe2c84661c4dc94dce7225
Smirnov Sergey <sesmirnov@mmtr.ru>
2021-12-08 11:56:19 +0300

    x-api-key header вынесен в конструктор для любого запроса
    fix #3

--- app/src/main/java/com/example/fragmentvm/network/RetrofitClient.kt
+++ app/src/main/java/com/example/fragmentvm/network/RetrofitClient.kt
@@ -1,7 +1,9 @@
 package com.example.fragmentvm.network
 
 import com.google.gson.GsonBuilder
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Request
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory
@@ -9,6 +11,7 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
+
 
 object RetrofitClient {
     private var retrofit: Retrofit? = null
@@ -20,8 +23,21 @@ object RetrofitClient {
 
         val gson = GsonBuilder().setLenient().create()
 
+        val requestInterceptor = Interceptor { chain ->
+            val original: Request = chain.request()
+
+            val requestBuilder: Request.Builder = original.newBuilder()
+                .header(
+                    "x-api-key",
+                    "6aad15c4-b124-4ec3-846c-2c76f69cf5e8"
+                )
+            val request: Request = requestBuilder.build()
+            chain.proceed(request)
+        }
+
         val client = OkHttpClient.Builder()
             .addInterceptor(logging)
+            .addInterceptor(requestInterceptor)
             .connectTimeout(1, TimeUnit.MINUTES)
             .readTimeout(1, TimeUnit.MINUTES)
             .writeTimeout(1, TimeUnit.MINUTES)
@@ -35,7 +51,6 @@ object RetrofitClient {
                 .addConverterFactory(ScalarsConverterFactory.create())
                 .addConverterFactory(GsonConverterFactory.create(gson))
                 .build()
-
         }
         return retrofit!!
     }
--- app/src/main/java/com/example/fragmentvm/network/RetrofitServices.kt
+++ app/src/main/java/com/example/fragmentvm/network/RetrofitServices.kt
@@ -11,34 +11,22 @@ import retrofit2.http.Query
 interface RetrofitServices {
 
     @GET("images/search")
-    @Headers(
-        "x-api-key: 6aad15c4-b124-4ec3-846c-2c76f69cf5e8",
-        "Content-Type: application/json"
-    )
+    @Headers("Content-Type: application/json")
     fun getCatList(): Call<MutableList<Cat>>
 
     @GET("images/search")
-    @Headers(
-        "x-api-key: 6aad15c4-b124-4ec3-846c-2c76f69cf5e8",
-        "Content-Type: application/json"
-    )
+    @Headers("Content-Type: application/json")
     fun getCatList2(): Call<LiveData<MutableList<Cat>>>
 
     @GET("images/search")
-    @Headers(
-        "x-api-key: 6aad15c4-b124-4ec3-846c-2c76f69cf5e8",
-        "Content-Type: application/json"
-    )
+    @Headers("Content-Type: application/json")
     fun getCatListRV(
         @Query("limit") limit: Int = 5,
         @Query("size") size: String = "small"
     ): Call<MutableList<Cat>>
 
     @GET("images/search")
-    @Headers(
-        "x-api-key: 6aad15c4-b124-4ec3-846c-2c76f69cf5e8",
-        "Content-Type: application/json"
-    )
+    @Headers("Content-Type: application/json")
     fun getFiveCats(
         @Query("limit") limit: Int = 15,
         @Query("size") size: String = "thumb"
@@ -48,14 +36,9 @@ interface RetrofitServices {
      * RxJava
      */
     @GET("images/search")
-    @Headers(
-        "x-api-key: 6aad15c4-b124-4ec3-846c-2c76f69cf5e8",
-        "Content-Type: application/json"
-    )
+    @Headers("Content-Type: application/json")
     fun getFiveCatsRxJava(
         @Query("limit") limit: Int = 15,
         @Query("size") size: String = "thumb"
     ): Single<List<Cat>>
-
-
 }
