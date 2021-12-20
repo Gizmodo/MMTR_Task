101c7c6ef0ffca9486b3c02e72cc23c3e89ea77f
Smirnov Sergey <sesmirnov@mmtr.ru>
2021-12-20 15:33:18 +0300

    Реализован RxJava
    fix #6

--- app/src/main/java/com/example/fragmentvm/di/RetroServiceInterface.kt
+++ app/src/main/java/com/example/fragmentvm/di/RetroServiceInterface.kt
@@ -1,7 +1,7 @@
 package com.example.fragmentvm.di
 
 import com.example.fragmentvm.model.Cat
-import retrofit2.Call
+import io.reactivex.rxjava3.core.Single
 import retrofit2.http.GET
 import retrofit2.http.Query
 
@@ -10,6 +10,6 @@ interface RetroServiceInterface {
     @GET("images/search")
     fun getCats(
         @Query("limit") limit: Int = 5,
-        @Query("size") size: String = "small"
-    ): Call<List<Cat>>
+        @Query("size") size: String = "small",
+    ): Single<List<Cat>>
 }
--- app/src/main/java/com/example/fragmentvm/repository/CatRepository.kt
+++ app/src/main/java/com/example/fragmentvm/repository/CatRepository.kt
@@ -3,26 +3,24 @@ package com.example.fragmentvm.repository
 import androidx.lifecycle.MutableLiveData
 import com.example.fragmentvm.di.RetroServiceInterface
 import com.example.fragmentvm.model.Cat
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
+import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers
+import io.reactivex.rxjava3.schedulers.Schedulers
 import timber.log.Timber
 import javax.inject.Inject
+
 
 class CatRepository @Inject constructor(private val apiService: RetroServiceInterface) {
     val getCats: MutableLiveData<List<Cat>>
         get() {
             val cats = MutableLiveData<List<Cat>>()
-            val response = apiService.getCats()
-            response.enqueue(object : Callback<List<Cat>> {
-                override fun onResponse(call: Call<List<Cat>>, response: Response<List<Cat>>) {
-                    cats.postValue(response.body())
-                }
-
-                override fun onFailure(call: Call<List<Cat>>, t: Throwable) {
-                    Timber.e(t.message)
-                }
-            })
+            apiService.getCats()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({ result ->
+                    cats.value = result
+                }, { error ->
+                    Timber.e(error)
+                })
             return cats
         }
 }
