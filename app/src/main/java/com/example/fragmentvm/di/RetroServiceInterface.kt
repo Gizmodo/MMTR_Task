b630b3c0243f567779a852af85e1e05d1b095ffd
Gizmodo <smirnovsergeyterem@gmail.com>
2021-12-29 00:35:23 +0300

    Рефакторинг
    Удалены неиспользуемые шрифты.
    Удален неиспользуемый код.
    Добавлено версионирование.

--- app/src/main/java/com/example/fragmentvm/di/RetroServiceInterface.kt
+++ app/src/main/java/com/example/fragmentvm/di/RetroServiceInterface.kt
@@ -4,17 +4,10 @@ import com.example.fragmentvm.model.Cat
 import com.example.fragmentvm.model.Payload
 import com.example.fragmentvm.model.Signup
 import io.reactivex.rxjava3.core.Observable
-import io.reactivex.rxjava3.core.Single
 import retrofit2.http.*
 
 
 interface RetroServiceInterface {
-    @GET("images/search")
-    fun getCats(
-        @Query("limit") limit: Int = 5,
-        @Query("size") size: String = "small",
-//        @Query("api_key") apiKey: String,
-    ): Single<List<Cat>>
 
     @GET("images/search")
     fun getCatsObservable(
