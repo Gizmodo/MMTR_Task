09795e469eace9645a1ae199f6b8901a8ca9b3ff
Smirnov Sergey <sesmirnov@mmtr.ru>
2022-01-28 19:01:42 +0300

    StateFlow: wip

--- app/src/main/java/com/example/fragmentvm/ui/MainFragment.kt
+++ app/src/main/java/com/example/fragmentvm/ui/MainFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.ViewModelProvider
@@ -16,6 +17,7 @@ import com.example.fragmentvm.databinding.MainFragmentBinding
 import com.example.fragmentvm.model.Cat
 import com.example.fragmentvm.utils.SharedViewModel
 import com.example.fragmentvm.viewmodel.MainViewModel
+import timber.log.Timber
 
 class MainFragment : Fragment() {
     companion object {
@@ -39,7 +41,6 @@ class MainFragment : Fragment() {
     ): View {
         binding = MainFragmentBinding.inflate(inflater, container, false)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
-
         viewModel.cats.observe(viewLifecycleOwner) { cats ->
             swipe.isRefreshing = false
             binding.recyclerview.also {
@@ -54,7 +55,30 @@ class MainFragment : Fragment() {
             swipe.isRefreshing = true
             viewModel.getCats()
         }
-
+viewModel.uiState.observe
+        when (val state = viewModel.uiState.value) {
+            MainViewModel.CatUiState.Empty -> {
+                Timber.d("Empty")
+                binding.progressBar.visibility = View.GONE
+            }
+            is MainViewModel.CatUiState.Error -> {
+                Timber.d("Error")
+                Toast.makeText(context, state.t.message, Toast.LENGTH_LONG)
+                    .show()
+            }
+            is MainViewModel.CatUiState.Loaded -> {
+                Timber.d("Loaded")
+                binding.progressBar.visibility = View.GONE
+            }
+            MainViewModel.CatUiState.Loading -> {
+                Timber.d("Loading")
+                binding.progressBar.visibility = View.VISIBLE
+            }
+            MainViewModel.CatUiState.Finished -> {
+                Timber.d("Finished")
+                binding.progressBar.visibility = View.GONE
+            }
+        }
         return binding.root
     }
 
--- app/src/main/java/com/example/fragmentvm/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/example/fragmentvm/viewmodel/MainViewModel.kt
@@ -3,15 +3,23 @@ package com.example.fragmentvm.viewmodel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.fragmentvm.App
 import com.example.fragmentvm.model.Cat
 import com.example.fragmentvm.repository.DataStoreRepositoryImpl
 import com.example.fragmentvm.repository.RepositoryRetrofit
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import timber.log.Timber
 import javax.inject.Inject
 
 class MainViewModel : ViewModel() {
+    private val _uiState = MutableStateFlow<CatUiState>(CatUiState.Empty)
+    val uiState: StateFlow<CatUiState> = _uiState
+
     init {
         App.instance().appGraph.embed(this)
         getCats()
@@ -28,8 +36,31 @@ class MainViewModel : ViewModel() {
         get() = _cats
 
     fun getCats() {
+        _uiState.value = CatUiState.Loading
         val apikey = runBlocking { ds.getString("apikey") }
-        repositoryRetrofit.getCats(apikey.toString())
-            .subscribe({ _cats.postValue(it) }, { Timber.e(it) })
+        viewModelScope.launch(Dispatchers.IO) {
+            repositoryRetrofit.getCats(apikey.toString())
+                .subscribe(
+                    {
+                        if (it.isEmpty()) {
+                            _uiState.value = CatUiState.Empty
+                        } else {
+                            _uiState.value = CatUiState.Finished
+                            _cats.postValue(it)
+                        }
+                    },
+                    {
+                        Timber.e(it)
+                        _uiState.value = CatUiState.Error(it)
+                    })
+        }
+    }
+
+    sealed class CatUiState {
+        object Empty : CatUiState()
+        object Loading : CatUiState()
+        object Finished : CatUiState()
+        class Loaded(val data: List<Cat>) : CatUiState()
+        class Error(val t: Throwable) : CatUiState()
     }
 }
