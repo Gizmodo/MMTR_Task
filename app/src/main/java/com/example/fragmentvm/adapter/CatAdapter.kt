c4200b8c3760e114716d5a51f63a4e9cb0bc832d
Smirnov Sergey <sesmirnov@mmtr.ru>
2022-01-31 17:29:26 +0300

    wip

--- app/src/main/java/com/example/fragmentvm/adapter/CatAdapter.kt
+++ app/src/main/java/com/example/fragmentvm/adapter/CatAdapter.kt
@@ -4,13 +4,18 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable
+import androidx.swiperefreshlayout.widget.CircularProgressDrawable.DEFAULT
 import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
+import com.example.fragmentvm.R
 import com.example.fragmentvm.databinding.RecyclerviewItemCatBinding
 import com.example.fragmentvm.model.Cat
 
+
 class CatAdapter(
     private val cats: List<Cat>,
-    private val listener: OnRecyclerViewItemClick
+    private val listener: OnRecyclerViewItemClick,
 ) :
     RecyclerView.Adapter<CatAdapter.MainViewHolder>() {
     override fun getItemCount() = cats.size
@@ -24,7 +29,24 @@ class CatAdapter(
 
     override fun onBindViewHolder(holder: MainViewHolder, position: Int) {
         val cat = cats[position]
-        Glide.with(holder.itemView.context).load(cat.url).into(holder.binding.imgView)
+        val circularProgressDrawable = CircularProgressDrawable(holder.itemView.context)
+        circularProgressDrawable.apply {
+            setStyle(DEFAULT)
+            setColorSchemeColors(R.color.error)
+            start()
+        }
+
+        val requestOptions = RequestOptions()
+        requestOptions.centerCrop()
+        requestOptions.placeholder(circularProgressDrawable)
+        requestOptions.error(R.drawable.ic_outline_running_with_errors_24)
+        requestOptions.fitCenter()
+
+        Glide
+            .with(holder.itemView.context)
+            .load(cat.url)
+            .apply(requestOptions)
+            .into(holder.binding.imgView)
         holder.binding.imgView.setOnClickListener {
             listener.onRecyclerViewItemClick(it, cat)
         }
