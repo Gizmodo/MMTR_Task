c65e634a84ba5b83e10bdebdd2597d2db3c8a676
Smirnov Sergey <sesmirnov@mmtr.ru>
2022-02-14 11:24:32 +0300

    DataStore вынесен во VM. flagReg вынесен в константы
    Fix #13
    Fix #14

--- app/src/main/java/com/example/fragmentvm/di/AppGraph.kt
+++ app/src/main/java/com/example/fragmentvm/di/AppGraph.kt
@@ -3,6 +3,7 @@ package com.example.fragmentvm.di
 import com.example.fragmentvm.MainActivity
 import com.example.fragmentvm.viewmodel.ApiViewModel
 import com.example.fragmentvm.viewmodel.LoginViewModel
+import com.example.fragmentvm.viewmodel.MainActivityVM
 import com.example.fragmentvm.viewmodel.MainViewModel
 import dagger.Component
 import javax.inject.Singleton
@@ -19,5 +20,6 @@ interface AppGraph {
     fun embed(vm: MainViewModel)
     fun embed(vm: ApiViewModel)
     fun embed(vm: LoginViewModel)
+    fun embed(vm: MainActivityVM)
     fun embed(activity: MainActivity)
 }
---
+++ app/src/main/java/com/example/fragmentvm/viewmodel/MainActivityVM.kt
@@ -0,0 +1,28 @@
+package com.example.fragmentvm.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.fragmentvm.App
+import com.example.fragmentvm.repository.data.DataStoreRepository
+import com.example.fragmentvm.utils.SingleLiveEvent
+import com.example.fragmentvm.utils.Util.FLAGREG
+import kotlinx.coroutines.runBlocking
+import javax.inject.Inject
+
+class MainActivityVM : ViewModel() {
+    @Inject
+    lateinit var dataStore: DataStoreRepository
+
+    private var _isAlreadyRegistered = SingleLiveEvent<Boolean>()
+    fun getIsAlreadyRegistered(): SingleLiveEvent<Boolean> = _isAlreadyRegistered
+
+    init {
+        App.instance().appGraph.embed(this)
+        getFlag()
+    }
+
+    private fun getFlag() = runBlocking {
+        dataStore.getBool(FLAGREG).let {
+            _isAlreadyRegistered.postValue(it)
+        }
+    }
+}
