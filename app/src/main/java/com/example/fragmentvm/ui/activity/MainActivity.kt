1c62a84f83645c754bd7912cb5e33d96da3f2104
Smirnov Sergey <sesmirnov@mmtr.ru>
2022-04-26 17:11:34 +0300

    Тестовый вариант навигации.
    Но он не устраивает... Надо попробовать вернуться к первоначальному варианту

--- app/src/main/java/com/example/fragmentvm/ui/activity/MainActivity.kt
+++ app/src/main/java/com/example/fragmentvm/ui/activity/MainActivity.kt
@@ -5,11 +5,13 @@ import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.fragmentvm.App
 import com.example.fragmentvm.R
 import com.example.fragmentvm.databinding.MainActivityBinding
 import com.example.fragmentvm.ui.fragments.LoginFragmentDirections
 import com.example.fragmentvm.ui.viewmodels.MainActivityViewModel
+import com.google.android.material.bottomnavigation.BottomNavigationView
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
@@ -24,16 +26,20 @@ class MainActivity : AppCompatActivity() {
         val model: MainActivityViewModel by viewModels()
         binding = MainActivityBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        val navHostFragment =
-            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navView: BottomNavigationView = binding.navView
+//        val navController = findNavController(R.id.nav_host_fragment)
+//        val navController= Navigation.findNavController(this,R.id.nav_graph )
+        navView.setupWithNavController(navHostFragment.findNavController())
         if (savedInstanceState == null) {
             model.getIsAlreadyRegistered().observe(this) {
                 when (it) {
                     true -> {
                         navHostFragment.findNavController()
+                            .navigate(LoginFragmentDirections.actionLoginFragmentToMainFragment())
+                        /*Переход к экрану с Избранным списком котов
                             .navigate(LoginFragmentDirections.actionLoginFragmentToFavouriteFragment())
-                        // TODO: Remove temp navigation to Favourite fragment
-//                            .navigate(LoginFragmentDirections.actionLoginFragmentToMainFragment())
+                         */
                     }
                     else -> Timber.i("NoAction")
                 }
